5e10f779b945970e7ea498c7bff9dd646e6c8731
沁源 <120715718+Azure-Source@users.noreply.github.com>
2026-08-12 16:21:31 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path'
 import { defineConfig } from '@lark-apaas/coding-preset-vite-react'
 
 export default defineConfig({
+  base: '/class2/', // 新增这一行！
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
